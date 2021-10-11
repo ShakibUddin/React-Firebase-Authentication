0e0958c4ca8e2a987c491ca4bcacaa39f07adfe2
ShakibUddin <shakibuddinbhuiyan@gmail.com>
2021-10-11 22:40:14 +0600

    form validation added

--- src/Components/SignInForm/SignInForm.js
+++ src/Components/SignInForm/SignInForm.js
@@ -1,31 +1,35 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useHistory } from "react-router-dom";
+import useEmailValidator from '../../Hooks/useEmailValidator';
+import usePasswordValidator from '../../Hooks/usePasswordValidator';
 import './SignInForm.css';
 
 const SignInForm = () => {
-    let [inputData, setInputData] = useState({
-        email: "",
-        password: ""
-    });
-    let [error, setError] = useState("");
-
+    let [email, handleEmailInput, emailError] = useEmailValidator();
+    let [password, , handlePasswordInput, passwordError] = usePasswordValidator();
     let history = useHistory();
 
     function handleClick() {
         history.push('./signup');
     }
-    function handleEmailInput(e) {
-        console.log(e.target.value);
-    }
-    function handlePasswordInput(e) {
-        console.log(e.target.value);
+
+    function handleFormSubmission(e) {
+        console.log(email, password);
+        e.preventDefault();
     }
     return (
-        <form className="form">
+        <form onSubmit={e => { handleFormSubmission(e) }} className="form">
             <h1>SignIn</h1>
-            <input type="text" name="" onBlur={e => { handleEmailInput(e) }} placeholder="Enter Email" />
-            <input type="password" name="" onBlur={e => { handlePasswordInput(e) }} placeholder="Enter Password" />
-            <button>Login</button>
+            <input required type="email" name="" onBlur={e => { handleEmailInput(e) }} placeholder="Enter Email" />
+            {
+                emailError.length > 0 && <p className="error-label">{emailError}</p>
+            }
+            <input required type="password" name="" onBlur={e => { handlePasswordInput(e) }} placeholder="Enter Password" />
+            {
+                passwordError.length > 0 && <p className="error-label">{passwordError}</p>
+            }
+            <input type="submit" value="LOGIN" />
+
             <p>Don't have an account? <span onClick={handleClick}>Register</span></p>
         </form>
     );
--- src/Components/SignUpForm/SignUpForm.js
+++ src/Components/SignUpForm/SignUpForm.js
@@ -1,42 +1,55 @@
 import React, { useState } from 'react';
 import { useHistory } from 'react-router';
+import useContactValidator from '../../Hooks/useContactValidator';
+import useEmailValidator from '../../Hooks/useEmailValidator';
+import usePasswordValidator from '../../Hooks/usePasswordValidator';
 
 const SignUpForm = () => {
-    let [inputData, setInputData] = useState({
-        name: "",
-        email: "",
-        contact: "",
-        password: ""
-    });
-    let [error, setError] = useState("");
+    let [email, handleEmailInput, emailError] = useEmailValidator();
+    let [password, setPassword, handlePasswordInput, passwordError] = usePasswordValidator();
+    let [contact, handleContactInput, contactError] = useContactValidator();
+    let [confirmPasswordError, setConfirmPasswordError] = useState("");
 
     let history = useHistory();
 
     function handleClick() {
-        history.push('./signin');
+        history.push('/signin');
     }
-    function handleEmailInput(e) {
-        console.log(e.target.value);
+
+    function handleConfirmPasswordInput(e) {
+        if (password !== e.target.value) {
+            setConfirmPasswordError("Passwords don't match");
+        }
+        else {
+            setPassword(e.target.value);
+            setConfirmPasswordError("");
+        }
     }
-    function handlePasswordInput(e) {
-        console.log(e.target.value);
-    }
-    function handleContactInput(e) {
-        console.log(e.target.value);
-    }
+
     function handleFormSubmit(e) {
-        console.log(e.target.value);
-        setInputData(e.target.value);
+        console.log(email, contact, password);
         e.preventDefault();
     }
     return (
         <form onSubmit={(e) => { handleFormSubmit(e) }} className="form">
             <h1>Sign Up</h1>
-            <input type="text" name="" onBlur={e => { handleEmailInput(e) }} placeholder="Enter Email" />
-            <input type="text" name="" onBlur={e => { handleContactInput(e) }} placeholder="Enter Contact Number" />
+            <input required minLength="3" maxLength="30" type="email" name="" onBlur={e => { handleEmailInput(e) }} placeholder="Enter Email" />
+            {
+                emailError.length > 0 && <p className="error-label">{emailError}</p>
+            }
+            <input required type="number" name="" onBlur={e => { handleContactInput(e) }} placeholder="Enter Contact Number" />
+            {
+                contactError.length > 0 && <p className="error-label">{contactError}</p>
+            }
             <input type="password" name="" onBlur={e => { handlePasswordInput(e) }} placeholder="Enter Password" />
-            <input type="password" name="" onBlur={e => { handlePasswordInput(e) }} placeholder="Confirm Password" />
-            <button>Create Account</button>
+            {
+                passwordError.length > 0 && <p className="error-label">{passwordError}</p>
+            }
+            <input required minLength="1" maxLength="20" type="password" name="" onBlur={e => { handleConfirmPasswordInput(e) }} placeholder="Confirm Password" />
+            {
+                confirmPasswordError.length > 0 && <p className="error-label">{confirmPasswordError}</p>
+            }
+            <input type="submit" value="Create Account" />
             <p>Already have an account? <span onClick={handleClick}>Login</span></p>
         </form>
     );
